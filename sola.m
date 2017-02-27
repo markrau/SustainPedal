a9857270c7d007f06875ce1af4516f1a7d2195f1
orchisama <orchisama.das@gmail.com>
2017-02-27 02:05:12 -0800

    Fixed strange humming sounds

--- sola.m
+++ sola.m
@@ -1,42 +1,36 @@
-function [y] = sola(xp, period)
-
-%without overlap
-% N = length(xp)
-% nperiod = ceil(N/period);
-% xp = [xp, zeros(1, period*nperiod - N)];
-% y = zeros(1, length(xp));
-% start = 1;
-% for n = 1:nperiod
-%     win = xp(start:start + period -1) .* hann(period)';
-%     y(start:start + period-1) = win;
-%     start = start + period;
-% end
-% y = y(1:N);
+function [y] = sola(xp, win_size, fs)
 
 %with overlap
-ola = 0.5;
-if(mod(period,2) == 0)
-    hop = ola * period;
-else
-    hop = ola * (period-1);
-end
+win_size = (2^nextpow2(win_size));
+%since I am using a hamming window here, COLA needs to have an overlap
+%factor of M/4 to prevent humming due to half of the main lobe aliasing
+%onto itself (decreasing hop size increases frame rate).
+%See Julius' book for reference on COLA
+ola = 0.25;
+hop = ola*(win_size);
+
 N = length(xp);
-nperiod = ceil(N/hop);
-xp = [xp, zeros(1,nperiod*hop - N + (period - hop))]; 
+nwin_size = ceil(N/hop);
+xp = [xp, zeros(1,nwin_size*hop - N + (win_size - hop))]; 
 y = zeros(1, length(xp));
-win = hamming(period)';
+win = hamming(win_size)';
 start = 1;
-for n = 1:nperiod
-    y(start:start + period - 1) = y(start:start + period - 1) + (xp(start:start + period - 1).*win);
+for n = 1:nwin_size
+    y(start:start + win_size - 1) = y(start:start + win_size - 1) + (xp(start:start + win_size - 1).*win);
     start = start + hop;
 end
-%need to get rid of trailing zeros at the end of y
-y = y(1:N);
 
+%need to get rid of :
+%1)one win_size at the beginning, because there is no overlap with previous frame there - 
+%we just have windowed waveform, which is not equal to the original, but smaller in
+%amplitude - this causes problems, so ignore it
+%2)trailing zeros at the end of y
+y = y(win_size+1:N)*mean(win);
 
 figure(3);
-plot(y(1:5*period));grid on;
-title('Reconstructed signal');
+plot(xp(1:10*win_size));grid on;hold on;
+plot(y(1:10*win_size), 'r-');grid on; hold off;
+legend('Original signal', 'Reconstructed Signal');
     
 end
 
--- test.m
+++ test.m
@@ -1,84 +1,76 @@
 close all, clc;
 
-[x,fs] = audioread('guitar files/G53-46201-1111-00015.wav');
+%try it with both audio files
+%[x,fs] = audioread('guitar files/G53-46201-1111-00015.wav');
+[x,fs] = audioread('guitar files/G53-50300-1111-00027.wav');
 x = x';
 win = 1024;
 nframes = ceil(length(x)/win);
 x = [x, zeros(1,nframes*win - length(x))];
 start = 1;   
-P0_est = zeros(1,nframes);
 time = zeros(1,nframes);
 xbuf = zeros(nframes, win);
-nsamples = zeros(1,nframes);
 
+%break into frames of 1024 samples
 for i = 1:nframes
     xbuf(i,:) = x(start:start+win-1);
-    P0_est(i) = yin_pitch(xbuf(i,:),fs);
     time(i) = (start-1)/fs;
     start = start + win;
-    nsamples(i) = round(P0_est(i)*fs);
-    %plot just to visually check if correct pitch was detected
-%     if(i == 50)
-%         figure(1);
-%         plot(xbuf(i,:),'b');grid on;hold on;
-%         plot(xbuf(i,1:nsamples(i)),'r');grid on;
-%         hold off;
-%     end
 end
 
-figure(1);
-plot(time,1./P0_est);grid on;
-xlabel('Time in seconds');ylabel('Estimated frequency');
-
-%extract one period of waveform and loop infinitely
-xp = [];
-%ideally this should be the steady state buffer number following onset
-%sorry for the misnomer.
-onset = 20;
-nperiods = 200;
+%ideally this should be the steady state buffer number following onset  
+steady_state = 50;
+nperiods = 500;
+%detect pitch period
+period = round(yin_pitch(xbuf(steady_state,:),fs)*fs);
 
 %extract exactly one period of fundamental starting from 0 phase,
 %loop that period for nperiods and send it to sola for overlap add
-period = nsamples(onset);
 xp = [];
-[m,maxind] = max(xbuf(onset,1:period));
-ind = find_zcr_before_peak(xbuf(onset,:), maxind);
+[m,maxind] = max(xbuf(steady_state,1:period));
+ind = find_zcr_before_peak(xbuf(steady_state,:), maxind);
 for i = 0:nperiods
-    xp = [xp, xbuf(onset,ind:ind+period)];
+    xp = [xp, xbuf(steady_state,ind:ind+period)];
 end
-y = sola(xp,period);
-        
-
-% period = mode(nsamples(onset:onset+nperiods));
-% %this next block of code basically does the following - 
-% %it extracts exactly one period from each buffer starting at 0 phase
-% %uncomment line 46 to see what it does
-% xp = [];
-% for i = 0:nperiods
-%     [m,maxind] = max(xbuf(onset+i,1:period));
-%     ind = find_zcr_before_peak(xbuf(onset+i,:), maxind);
-%     figure(2);plot(xbuf(onset+i,ind:ind+period));hold on; grid on;
-%     xp = [xp, xbuf(onset+i,ind:ind+period)];
-% end
-% figure(2);hold off;
-% y = sola(xp,period);
-%let's hear what the reconstructed SOLA signal sounds like
-%soundsc(y,fs);
-
-%let's hear what it sounds like combined with the original signal
-xcons = zeros(1, (onset-1)*win);
+y = sola(xp,period,fs);
+   
+%initial part of reconstructed signal - attack is going to come from
+%original signal
+xcons = [];
 start = 1;
-for n = 1:onset-1
+for n = 1:steady_state-1 
     xcons(start:start+win-1) = xbuf(n,:);
     start = start+win;
 end
-%linear interpolation between last sample in onset and first sample in
-%steady state for smooth transition
-interp = linspace(xcons(end), y(1), 100);
-z = [xcons, interp, y];
-sound(z,fs);
 
-%audio sounds a bit 
+%this maintains continuity between initial (steady_state) and looped(steady state) regions
+%leading to a smooth transition
+th = 10^-5;
+for i = length(xbuf(steady_state,:)) : -1 : 2
+    if(abs(xbuf(steady_state,i) - y(1)) < th)  
+        xcons(start:start+i-2) = xbuf(steady_state,1:i-1);
+        break;
+    end
+end
+
+%multiply y with a slowly varying amplitude envelope - amplitude modulation
+%makes it sound more like a real guitar signal
+t = (0:length(y)-1)/fs;
+mod_ind = 0.05;
+ys = (1 + mod_ind*sin(2*pi*2.*t)).*y;
+z = [xcons, ys];
+
+%multiply end of reconstructed signal (last few periods) with linearly decaying window
+lin_dec = linspace(1, 0, (100*period));
+z = [z(1:end-length(lin_dec)), z(end-length(lin_dec)+1:end).*lin_dec];
+
+soundsc(z,fs);
+figure;
+subplot(211);
+plot(x);title('Original signal');
+subplot(212);plot(z);
+title('Reconstructed signal');
+    
 
 %In real time C++, we will ideally keep one fundamental period of the
 %waveform stored in an array and keep looping that in the output buffer,
