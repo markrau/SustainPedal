e0003285af2205dd6f0d57f4ab383b2f6a4f564e
Mark Rau <markrau@DN51sc2h.SUNet>
2017-03-07 01:08:12 -0800

    trying to fix

--- Sustain/Sustain.ino
+++ Sustain/Sustain.ino
@@ -32,7 +32,7 @@ const int maxDataLength = 2048;
 const long Gain         = 32768; ///< Gain parameter
 const long baudRate      = 115200;
 //const int maxBuffsAfterOnset =5;                   // number of buffers to wait before looking for new onset after one is detected
-const int maxBuffUntilSteadyState = 25;          //  number of buffers after onset until it is declared as steady state
+const int maxBuffUntilSteadyState = 15;          //  number of buffers after onset until it is declared as steady state
 
 
 /// Create instance of the serial command class
@@ -51,7 +51,7 @@ int period = 0;
 int numOnsets = 0;
 =======
 int prevOnsetFlag = 0; // Keep track of whether or not a buffer has an onset
-int onsetThresh = MAX_INT16*0.25 ;    // Onset detection threshold. Initialize to 4 (Q11), meaning that an onset is detected if a new buffer has 4 times the spectral energy as the previous buffer
+int onsetThresh = MAX_INT16*0.125 ;    // Onset detection threshold. Initialize to 4 (Q11), meaning that an onset is detected if a new buffer has 4 times the spectral energy as the previous buffer
 //int numBuffsAfterOnset = 0;
 int numBuffUntilSteadyState = 0;
 int period = 0;
@@ -121,14 +121,11 @@ void setup()
     // Set pin as output
     pinMode(LED0, OUTPUT);
     pinMode(LED2, OUTPUT);
-    pinMode(LED1, OUTPUT);
     
     
     // Turn LED0 on
     digitalWrite(LED0, HIGH);
     digitalWrite(LED2, LOW);
-    digitalWrite(LED1, LOW);
-    
 
     //Initialize OLED module for status display	
     disp.oledInit();
@@ -200,6 +197,7 @@ void loop()
         digitalWrite(LED2, LOW);
     }
 <<<<<<< HEAD
+<<<<<<< HEAD
     if(readyToProcess){
       disp.clear();
       disp.setline(0);
@@ -211,13 +209,14 @@ void loop()
     }
     else{
      digitalWrite(LED1, LOW); 
+=======
+    if(readyToProcess){
+      //period = extract.hps_pitch(InputLeft, 4);
+      disp.clear();
+      disp.setline(0);
+      disp.print((long)period);
+>>>>>>> parent of 5293257... some rough looping
     }
-//    if(readyToProcess){
-//      //period = extract.hps_pitch(InputLeft, 4);
-//      disp.clear();
-//      disp.setline(0);
-//      disp.print((long)period);
-//    }
     
 
    if(numMatlabCalls <1){
@@ -327,7 +326,7 @@ void processAudio()
 =======
            
         //period = extract.yin_pitch(InputLeft);
-        //period = extract.fft_pitch(InputLeft);
+        period = extract.fft_pitch(InputLeft);
         //period = extract.hps_pitch(InputLeft,4);
         /*copyShortBuf(AudioC.inputLeft,OutputLeft, BufferLength);
         copyShortBuf(AudioC.inputRight,OutputRight, BufferLength);*/
@@ -349,7 +348,7 @@ void processAudio()
       for(int n = 0; n < BufferLength; n++) {
         AudioC.outputLeft[n]  = (Gain * InputLeft[n])  >> 15;
         AudioC.outputRight[n] = (Gain * InputRight[n]) >> 15;
-      }  
+      }
     }
     else{
       for(int n = 0; n < BufferLength; n++) {
@@ -371,8 +370,7 @@ void processAudio()
         digitalWrite(LED0, LOW);
         for(int n = 0; n <BufferLength; n++)
         {
-            //AudioCaptureBufferLeft[n]  = AudioC.inputLeft[n];
-            AudioCaptureBufferLeft[n]  = InputLeft[n];
+            AudioCaptureBufferLeft[n]  = AudioC.inputLeft[n];
         }
         GetAudioBufferFlag = 0;
         CapturedBufferFlag = 1;
