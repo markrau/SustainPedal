4d5d637e0bf3bc7fb1e01aff17f6354b50803d3c
orchidas <orchisama.das@gmail.com>
2017-03-07 08:50:57 -0800

    Added folder

--- Sustain/ExtractFundamental.h
+++ Sustain/ExtractFundamental.h
@@ -3,33 +3,23 @@
 #ifndef ___ExtractFundamental__
 #define ___ExtractFundamental__
 
-#include<stdio.h>
 #include<OLED.h>
-#include <serial_array.h>
 #include "dsplib.h"
 
 class ExtractFundamental{
 	public:
 		ExtractFundamental(int bufLength,long Fs);
 		~ExtractFundamental();
-<<<<<<< HEAD
-                int acorr_pitch(int *buffer, int* xcorr, int thresh);
-=======
-//<<<<<<< HEAD
-//		int yin_pitch(int* buffer);
-//		int* get_fundamental();
-//=======
                 int acorr_pitch(int *buffer, int thresh);
->>>>>>> 5293257a6390632d22f7f42ddbca00fedf69864b
+
                 int fft_pitch(int *buffer);
                 int hps_pitch(int *buffer, long* mag, int nharmonics);
-		int yin_pitch(int *buffer, int* diff, int* d_norm);
-		void get_fundamental(int *input, int* one_period, int* output);
+				int yin_pitch(int *buffer, int* diff, int* d_norm);
+				void get_fundamental(int *input, int* one_period, int* output);
 
-                //to communicate with MATLAB 
-                SerialCmd cmd;
+            
                 
-//>>>>>>> 4523fbfe8dfe930b45a1e7ffa38adced65e6a6ea
+
 		
 	private:
 		int buf_len;
